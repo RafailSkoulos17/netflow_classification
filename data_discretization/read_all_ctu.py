5a7ee64981983771ecd9137fd23553074f1b715a
Rafail Skoulos <rafail.sk17@hotmail.gr>
2020-04-14 21:53:59 +0200

    Added root dir for imports

--- data_discretization/read_all_ctu.py
+++ data_discretization/read_all_ctu.py
@@ -1,5 +1,10 @@
 import os
+import inspect
 import pandas as pd
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
 from pypackage.flexfringe.utils import preprocess_bidirectional_data, remove_background
 
 
